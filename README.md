5865d97313c709fa56df0be4088282148d9f939e
Leslie_Jiang <Leslie_Jiang@proton.me>
2024-12-04 18:11:13 +0800

    test: cat pic

--- README.md
+++ README.md
@@ -1,11 +1,49 @@
 # Installation
 
-First, set up a new conda environment and activate it.
+## Clone this repo
 
-Then run:
+```bash
+git clone https://github.com/Leslie-Jiang-Hamster/clip
+cd clip
+```
+
+## Installing conda
+
+```bash
+curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+```
+
+Enter a lot of yes, then you'll see "Thank you for installing Miniconda3!"
+
+```bash
+source ~/.bashrc
+```
+
+Test your installation by running `conda list`. If conda has been installed correctly, a list of installed packages appears.
+
+Then create an environment called clip:
+
+```bash
+conda create -n clip python=3.9
+conda activate clip
+```
+
+## Installing huggingface transformers
+
+(Ensure that you are in the "clip" conda environment) Run:
 
 ```bash
 conda install pip
 pip install 'transformers[torch]'
 pip install pillow
 ```
+
+## Test
+
+```bash
+python clip.py
+```
+
+This program will predict if the image "cat.jpg" is a cat, a dog or a horse.
+If the output is cat, then we are finished.
--- clip.py
+++ clip.py
@@ -2,15 +2,23 @@ from PIL import Image
 
 from transformers import CLIPProcessor, CLIPModel
 
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+class Classifier:
+  def __init__(self, categories):
+    self.model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+    self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+    self.categories = categories
+  
+  def predict(self, image):
+    inputs = self.processor(text=self.categories, images=image, return_tensors="pt", padding=True)
+    outputs = self.model(**inputs)
+    logits_per_image = outputs.logits_per_image
+    probs = logits_per_image.softmax(dim=1)
+    category = self.categories[probs.argmax()]
+    return category
+
+classifier = Classifier(["cat", "dog", "elephant"])
 
 image = Image.open("cat.jpg")
 
-inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True)
+print(classifier.predict(image))
 
-outputs = model(**inputs)
-logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
-probs = logits_per_image.softmax(dim=1)  # we can take the softmax to get the label probabilities
-
-print(probs)
